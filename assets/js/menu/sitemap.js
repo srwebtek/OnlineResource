887994a310e52609888cc36471e4da4a984393e9
srwebtek <srwebtek@gmail.com>
2023-08-14 22:58:05 +1000

    made pages in sitemap ready for content

--- assets/js/menu/sitemap.js
+++ assets/js/menu/sitemap.js
@@ -1,28 +1,76 @@
 const contentSitemap = {
-    Item1: {
-      link: "#",
-      title: "Sitemap:Item 1",
-    },
-    Item2: {
-      link: "#",
-      title: "Sitemap:Item 2",
-    },
-    Item3: {
-      link: "#",
-      title: "Sitemap:Item 3",
-    },
-    Item4: {
-      link: "#",
-      title: "Sitemap:Item 4",
-    },
-    Item5: {
-      link: "#",
-      title: "Sitemap:Item 5",
-    },
-    Item6: {
-      link: "#",
-      title: "Sitemap:Item 6",
-    },
-  };
-  
-  export default contentSitemap;
+  Item1: {
+    link: "/sitemap/are-videos-included-in-XML-sitemaps.html",
+    title: "Are videos included in XML sitemaps?",
+  },
+  Item2: {
+    link: "/sitemap/do-small-websites-need-a-sitemap.html",
+    title: "Do small websites need a sitemap ",
+  },
+  Item3: {
+    link: "/sitemap/does-a-website-need-to-have-a-sitemap.html",
+    title: "does a website need to have a sitemap?",
+  },
+  Item4: {
+    link: "/sitemap/how-do-i-create-an-image-sitemap.html",
+    title: "how do i create an image sitemap?",
+  },
+  Item5: {
+    link: "/sitemap/how-many-pages-are-in-a-sitemap.html",
+    title: "how many pages are in a sitemap?",
+  },
+  Item6: {
+    link: "/sitemap/is-a-sitemap-free.html",
+    title: "is a sitemap free?",
+  },
+  Item7: {
+    link: "/sitemap/is-a-sitemap-still-relevant.html",
+    title: "is a sitemap still relevant?",
+  },
+  Item8: {
+    link: "/sitemap/is-a-sitemap-XML-or-HTML.html",
+    title: "is a sitemap XML or HTML?",
+  },
+  Item9: {
+    link: "/sitemap/is-a-sitemap-important-for-SEO.html",
+    title: "is a sitemap important for SEO?",
+  },
+  Item10: {
+    link: "/sitemap/should-i-exclude-images-from-the-sitemap.html",
+    title: "should i exclude images from a sitemap?",
+  },
+  Item11: {
+    link: "/sitemap/should-images-be-included-in-a-sitemap.html",
+    title: "should images be included in a sitemap?",
+  },
+  Item12: {
+    link: "/sitemap/what-is-the-sitemap-URL.html",
+    title: "what is the sitemap URL?",
+  },
+  Item13: {
+    link: "/sitemap/what-is-the-structure-of-a-sitemap.html",
+    title: "what is the structure of a sitemap?",
+  },
+  Item14: {
+    link: "/sitemap/what-is-the-use-of-a-sitemap-in-SEO.html",
+    title: "what is the use of a sitemap in SEO?",
+  },
+  Item15: {
+    link: "/sitemap/what-should-be-included-in-a-sitemap.html",
+    title: "what should be included in a sitemap?",
+  },
+  Item16: {
+    link: "/sitemap/where-can-i-create-a-sitemap-for-free.html",
+    title: "where can i create a sitemap for free?",
+  },
+  Item17: {
+    link: "/sitemap/where-do-i-find-a-sitemap.html",
+    title: "where do i find a sitemap?",
+  },
+  Item18: {
+    link: "/sitemap/where-do-i-put-my-sitemap.html",
+    title: "where do i put my sitemap?",
+  },
+};
+
+export default contentSitemap;
