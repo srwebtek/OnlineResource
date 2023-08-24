6a093456bb0149ef3dbb1a32baaa1c68c189c4d5
srwebtek <srwebtek@gmail.com>
2023-08-24 18:44:15 +1000

    comment out pages with no content

--- assets/js/menu/sitemap.js
+++ assets/js/menu/sitemap.js
@@ -23,54 +23,54 @@ const contentSitemap = {
     link: "/sitemap/how-to-submit-your-sitemap-to-search-engines.html",
     title: "How to Submit Your Sitemap to Search Engines ",
   },
-  Item6: {
-    link: "/sitemap/importance-of-sitemaps-for-seo.html",
-    title: "Importance of Sitemaps for SEO",
-  },
-  Item7: {
-    link: "/sitemap/is-a-sitemap-free-to-create.html",
-    title: "Is a Sitemap Free to Create?",
-  },
-  Item8: {
-    link: "/sitemap/is-a-sitemap-still-relevant-for-modern-websites.html",
-    title: "Is a Sitemap Still Relevant for Modern Websites?",
-  },
-  Item9: {
-    link: "/sitemap/is-a-sitemap-xml-or-html.html",
-    title: "Is a Sitemap XML or HTML?",
-  },
-  Item10: {
-    link: "/sitemap/should-i-exclude-images-from-my-sitemap.html",
-    title: "Should I Exclude Images from My Sitemap?",
-  },
-  Item11: {
-    link: "/sitemap/should-images-be-included-in-a-sitemap.html",
-    title: "Should Images Be Included in a Sitemap?",
-  },
-  Item12: {
-    link: "/sitemap/what-is-the-structure-of-a-sitemap.html",
-    title: "What Is the Structure of a Sitemap?",
-  },
-  Item13: {
-    link: "/sitemap/what-information-should-be-included-in-a-sitemap.html",
-    title: "What Information Should Be Included in a Sitemap?",
-  },
-  Item14: {
-    link: "/sitemap/what-is-an-xml-sitemap.html",
-    title: "What is an XML Sitemap?",
-  },
-  Item15: {
-    link: "/sitemap/where-can-i-create-a-sitemap-for-free.html",
-    title: "Where Can I Create a Sitemap for Free?",
-  },
-  Item16: {
-    link: "/sitemap/where-should-i-put-my-sitemap-on-my-website.html",
-    title: "Where Should I Put My Sitemap on My Website",
-  },
-  Item17: {
-    link: "/sitemap/where-to-find-your-sitemap-on-a-website.html",
-    title: "Where to Find Your Sitemap on a Website",
-  },
+  // Item6: {
+  //   link: "/sitemap/importance-of-sitemaps-for-seo.html",
+  //   title: "Importance of Sitemaps for SEO",
+  // },
+  // Item7: {
+  //   link: "/sitemap/is-a-sitemap-free-to-create.html",
+  //   title: "Is a Sitemap Free to Create?",
+  // },
+  // Item8: {
+  //   link: "/sitemap/is-a-sitemap-still-relevant-for-modern-websites.html",
+  //   title: "Is a Sitemap Still Relevant for Modern Websites?",
+  // },
+  // Item9: {
+  //   link: "/sitemap/is-a-sitemap-xml-or-html.html",
+  //   title: "Is a Sitemap XML or HTML?",
+  // },
+  // Item10: {
+  //   link: "/sitemap/should-i-exclude-images-from-my-sitemap.html",
+  //   title: "Should I Exclude Images from My Sitemap?",
+  // },
+  // Item11: {
+  //   link: "/sitemap/should-images-be-included-in-a-sitemap.html",
+  //   title: "Should Images Be Included in a Sitemap?",
+  // },
+  // Item12: {
+  //   link: "/sitemap/what-is-the-structure-of-a-sitemap.html",
+  //   title: "What Is the Structure of a Sitemap?",
+  // },
+  // Item13: {
+  //   link: "/sitemap/what-information-should-be-included-in-a-sitemap.html",
+  //   title: "What Information Should Be Included in a Sitemap?",
+  // },
+  // Item14: {
+  //   link: "/sitemap/what-is-an-xml-sitemap.html",
+  //   title: "What is an XML Sitemap?",
+  // },
+  // Item15: {
+  //   link: "/sitemap/where-can-i-create-a-sitemap-for-free.html",
+  //   title: "Where Can I Create a Sitemap for Free?",
+  // },
+  // Item16: {
+  //   link: "/sitemap/where-should-i-put-my-sitemap-on-my-website.html",
+  //   title: "Where Should I Put My Sitemap on My Website",
+  // },
+  // Item17: {
+  //   link: "/sitemap/where-to-find-your-sitemap-on-a-website.html",
+  //   title: "Where to Find Your Sitemap on a Website",
+  // },
   
 };
 
